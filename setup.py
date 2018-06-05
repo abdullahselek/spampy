1428e22cf4deb055710e23fc2365b141940a495e
Abdullah Selek <abdullahselek@gmail.com>
2018-06-05 22:20:48 +0100

    Add required files for module setup.

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import os
+import re
+import codecs
+
+from setuptools import setup, find_packages
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+def read(filename):
+    with codecs.open(os.path.join(cwd, filename), 'rb', 'utf-8') as h:
+        return h.read()
+
+metadata = read(os.path.join(cwd, 'spampy', '__init__.py'))
+
+def extract_metaitem(meta):
+    meta_match = re.search(r"""^__{meta}__\s+=\s+['\"]([^'\"]*)['\"]""".format(meta=meta),
+                           metadata, re.MULTILINE)
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
+
+setup(
+    name='spampy',
+    version=extract_metaitem('version'),
+    license=extract_metaitem('license'),
+    description=extract_metaitem('description'),
+    long_description=(read('README.rst')),
+    author=extract_metaitem('author'),
+    author_email=extract_metaitem('email'),
+    maintainer=extract_metaitem('author'),
+    maintainer_email=extract_metaitem('email'),
+    url=extract_metaitem('url'),
+    download_url=extract_metaitem('download_url'),
+    packages=find_packages(exclude=('tests', 'docs')),
+    include_package_data=True,
+    platforms=['Any'],
+    install_requires=['scikit_learn', 'scipy', 'numpy', 'stemming'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    keywords='machine learning, spam filter, support vector machine, spam, svm',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
---
+++ spampy/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''Spam filtering module with Machine Learning using SVM.'''
+
+from __future__ import absolute_import
+
+__author__       = 'Abdullah Selek'
+__email__        = 'abdullahselek@gmail.com'
+__copyright__    = 'Copyright (c) 2018 Abdullah Selek'
+__license__      = 'MIT License'
+__version__      = '0.1'
+__url__          = 'https://github.com/abdullahselek/spampy'
+__download_url__ = 'https://github.com/abdullahselek/spampy'
+__description__  = 'Spam filtering module with Machine Learning using SVM.'
