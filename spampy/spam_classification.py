34c6580780fd726cef5899c5b1bf0f7cda471bf9
Abdullah Selek <abdullahselek@gmail.com>
2018-06-08 15:47:14 +0100

    Train support vector machine and predict given raw email.

--- spampy/spam_classification.py
+++ spampy/spam_classification.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import scipy.io as sio
+import numpy as np
 
 from os.path import join, dirname
+from sklearn import svm
+from spampy import email_processor
 
 # Parent directory
 parent_directory_path = dirname(__file__)
+# Support Vector Machine
+linear_svm = svm.SVC(C=0.1, kernel='linear')
 
 def load_training_set():
     """
@@ -32,3 +37,27 @@ def load_test_set():
     dataset = sio.loadmat(training_set)
     Xtest, ytest = dataset['Xtest'], dataset['ytest']
     return Xtest, ytest
+
+def train_svm():
+    """
+    Fit SVM with features and labels.
+    """
+    X, y = load_training_set()
+    linear_svm.fit(X, y.flatten())
+    return X, y
+
+def predict_email(email):
+    """
+    Predict spam possibility of given email.
+    Args:
+      email (str):
+        Raw e-mail.
+    """
+
+    _, y = train_svm()
+    vocablary_dict = email_processor.get_vocablary_dict()
+    feature_vector = email_processor.feature_vector_from_email(email, vocablary_dict)
+    double_dimesion_email = np.reshape(feature_vector, (-1, 1899))
+    spam_prediction = linear_svm.predict(double_dimesion_email)
+    spam_acc = 100. * float(sum(spam_prediction == y))/y.shape[0]
+    return 'Spam possibility = %0.2f%%' % spam_acc
--- tests/test_spam_classification.py
+++ tests/test_spam_classification.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import os
+
 from spampy import spam_classification
 
 class SpamClassificationTests(unittest.TestCase):
@@ -15,3 +17,8 @@ class SpamClassificationTests(unittest.TestCase):
         Xtest, ytest = spam_classification.load_test_set()
         self.assertIsNotNone(Xtest)
         self.assertIsNotNone(ytest)
+
+    def test_predict_email(self):
+        with open(os.path.join('tests/data', 'spam_sample.txt'), 'r') as f:
+            prediction = spam_classification.predict_email(f.read())
+            self.assertEqual(prediction, 'Spam possibility = 68.08%')
