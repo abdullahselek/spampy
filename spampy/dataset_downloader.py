e0f419651aa73108971c158308731ad7e260a770
Abdullah Selek <abdullahselek@gmail.com>
2020-01-14 02:47:41 +0000

    Refactoring on dataset downloader.

--- spampy/dataset_downloader.py
+++ spampy/dataset_downloader.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import subprocess
 import os
-import sys
+
 
 def download_enron_dataset():
     script_path = os.path.join('spampy', 'dataset_downloader.sh')
-    subprocess.call([script_path])
+    os.system(script_path)
