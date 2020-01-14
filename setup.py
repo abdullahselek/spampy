8a213711bc093636c36cec650023fabb3a532465
Abdullah Selek <abdullahselek@gmail.com>
2020-01-14 02:50:11 +0000

    Remove Python 3.4 support.

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ setup(
         'Topic :: Software Development',
         'Topic :: Scientific/Engineering',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
