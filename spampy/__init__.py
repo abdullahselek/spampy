4ed030d3f7ba0dde235318bc8a47e5f0a236be49
Abdullah Selek <abdullahselek@gmail.com>
2020-01-14 02:40:47 +0000

    Update download url.

--- spampy/__init__.py
+++ spampy/__init__.py
@@ -10,7 +10,7 @@ __copyright__    = 'Copyright (c) 2018 Abdullah Selek'
 __license__      = 'MIT License'
 __version__      = '0.1.0'
 __url__          = 'https://github.com/abdullahselek/spampy'
-__download_url__ = 'https://github.com/abdullahselek/spampy'
+__download_url__ = 'https://pypi.org/project/spampy/'
 __description__  = 'Spam filtering module with Machine Learning using SVM.'
 
 from spampy import (
