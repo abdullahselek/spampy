55fed3577d99c38f070ac6b7a6b03e2550a42fa6
Abdullah Selek <abdullahselek@gmail.com>
2020-04-25 20:44:09 +0100

    Add docs on downloader

--- spampy/dataset_downloader.py
+++ spampy/dataset_downloader.py
@@ -5,5 +5,9 @@ import os
 
 
 def download_enron_dataset():
+    """
+    Downloads Enron dataset.
+    """
+
     script_path = os.path.join('spampy', 'dataset_downloader.sh')
     os.system(script_path)
