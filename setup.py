315020d50aaf73361c99e9466825ac10d865243d
Abdullah Selek <abdullahselek@gmail.com>
2020-06-06 19:40:25 +0100

    Add long content description type

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     license=extract_metaitem('license'),
     description=extract_metaitem('description'),
     long_description=(read('README.rst')),
+    long_description_content_type='text/x-rst',
     author=extract_metaitem('author'),
     author_email=extract_metaitem('email'),
     maintainer=extract_metaitem('author'),
